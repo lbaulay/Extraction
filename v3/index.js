316a9bbb45f026c69c5df63e622e2b83844fd9e5
Laurent Baulay <lbaulay@dsi-dyn-178.enst.fr>
2017-06-13 10:52:30 +0200

    Suppression du onclick, remplacer par un listener

--- v3/index.js
+++ v3/index.js
@@ -9,32 +9,31 @@ $( function() {
     });
 });
 
-function onClick(id){ //changer le nom
-    console.log(typeof id+ "-"+ id);
-    var elem = document.getElementById(id);
-    if (elem.className == "fichier") {
-            /* Si tabCSV existe deja on le supprime avant d'afficher une nouvelle table*/
-            if (document.getElementsByClassName("tabCSV").length >0) {
-                document.getElementsByClassName("affichageInde")[0].removeChild(document.getElementsByClassName("tabCSV")[0]);
-            }
-            /* On creer le tableau de previsualisation */
-            var parent = elem.parentNode.parentNode.id;
-            Papa.parse(URL+"/"+id, {
-            download: true,
-            complete: function(results) {
-                var div = document.getElementsByClassName("affichageInde")[0];
-                var nomFichier = id.split("/")[1];
-                var texte = 'Ouvrir le fichier <a href="'+URL+"/"+id+'">' + parent+' - '+nomFichier+'</a></p>';
-                document.getElementById("titreAffichage").innerHTML = texte;
-                var table = ArrayToTab(results.data);
-                /* On ajoute le tableau au DOM, en enfant de <div affichageInde */
-                
-                div.appendChild(table);
-            }
-        });
+$(".fichier").on("click",function(){
+    var elem = $(this);
+    var nomFichier = elem.data("nom-fichier");
+    /* Si tabCSV existe deja on le supprime avant d'afficher une nouvelle table*/
+    if (document.getElementsByClassName("tabCSV").length >0) {
+        document.getElementsByClassName("affichageInde")[0].removeChild(document.getElementsByClassName("tabCSV")[0]);
     }
-}
+    /* On creer le tableau de previsualisation */
+    var date = elem.attr("data-date");
+    
+    Papa.parse(URL+"/"+date+"/"+nomFichier, {
+        download: true,
+        complete: function(results) {
+            var div = document.getElementsByClassName("affichageInde")[0];
+            
+            var texte = 'Ouvrir le fichier <a href="'+URL+"/"+date+"/"+nomFichier+'">' + date+' - '+nomFichier+'</a></p>';
+            document.getElementById("titreAffichage").innerHTML = texte;
+            var table = ArrayToTab(results.data);
+            /* On ajoute le tableau au DOM, en enfant de <div affichageInde */
 
+            div.appendChild(table);
+        }
+    });
+    
+});
 function ArrayToTab(arrayData) {
     var table = document.createElement("table");
     table.setAttribute("class","tabCSV");
@@ -55,3 +54,4 @@ function ArrayToTab(arrayData) {
     table.innerHTML += line;
     return table;
 }
+
--- v3/index.php
+++ v3/index.php
@@ -8,57 +8,64 @@
     </head>
     <body>
         <div id="date">
-<?php
-define("URL", "../date"); // On part du repertoire /date et on recherche les sous dossiers
-
+            <?php
+            define("URL", "../date"); // On part du repertoire /date et on recherche les sous dossiers
 // On crée l'arborescence
-if ($dossier = opendir(URL)) :
-    echo '<ul id="accordion">';
-    $lstDir = array(); // Liste des dossiers trouvé utiliser pour afficher les dossier dans l'ordre alphabetique
-    while (false !== ($fichier = readdir($dossier))) :
-        if ($fichier == '.' || $fichier == '..') {
-            continue;
-        }
-        $urlfichier = URL.'/'.$fichier;
-        if (is_dir($urlfichier)) {
-            $lstDir[]=$fichier;
-        }
-    endwhile;
-    closedir($dossier);
-    sort($lstDir);
+            if ($dossier = opendir(URL)) :
+                ?>
+                <ul id="accordion">
+                <?php
+                    $lstDir = array(); // Liste des dossiers trouvé utiliser pour afficher les dossier dans l'ordre alphabetique
+                    while (false !== ($fichier = readdir($dossier))) :
+                        if ($fichier == '.' || $fichier == '..') {
+                            continue;
+                        }
+                        $urlfichier = URL . '/' . $fichier;
+                        if (is_dir($urlfichier)) {
+                            $lstDir[] = $fichier;
+                        }
+                    endwhile;
+                    closedir($dossier);
+                    sort($lstDir);
 
-    foreach ($lstDir as $fichier) : // On parcours la liste de dossier
-        $urlfichier = URL.'/'.$fichier;
-        ?>
-        <li class="arbo" id="li<?= $fichier?>"><a href="#"><?=$fichier?></a>
-        <?php
-        if ($sousDossier = opendir($urlfichier)) :
-            $listeFichierCSV = [];
-            // On ouvre les sous dossier et on affiche les fichiers qui y sont présent
-            ?>
-            <ul class="sousDossier" id="<?=$fichier?>">
-            <?php
-            while (false !==($fichierExtrait = readdir($sousDossier))) {
-                // on ne liste que les fichiers .csv
+                    foreach ($lstDir as $RepDate) : // On parcours la liste de dossier
+                        $urlfichier = URL . '/' . $RepDate;
+                        ?>
+                        <li class="arbo" id="li<?= $RepDate ?>"><a href="#"><?= $RepDate ?></a>
+                            <?php
+                            if ($sousDossier = opendir($urlfichier)) :
+                                $listeFichierCSV = []; // On ouvre les sous dossier et on affiche les fichiers qui y sont présent
+                                ?>
 
-                if (is_file($urlfichier.'/'.$fichierExtrait) && "csv" === substr($fichierExtrait, -3)) {
-                    $listeFichierCSV[]=$fichierExtrait;
-                }
-            }
-            foreach ($listeFichierCSV as $fichierCSV) :?>
-                <li><a class="fichier" id ="<?=$fichier."/".$fichierCSV?>" onclick ="onClick('<?=$fichier."/".$fichierCSV?>')"><?=$fichierCSV?></a></li>
+
+                                <ul class="sousDossier" id="<?= $RepDate ?>">
+                                    <?php
+                                    while (false !== ($fichierExtrait = readdir($sousDossier))) {
+                                        // on ne liste que les fichiers .csv
+
+                                        if (is_file($urlfichier . '/' . $fichierExtrait) && "csv" === substr($fichierExtrait, -3)) {
+                                            $listeFichierCSV[] = $fichierExtrait;
+                                        }
+                                    }
+                                    foreach ($listeFichierCSV as $fichierCSV) :
+                                        ?>
+
+                                        <li><a class="fichier" data-date="<?=$RepDate?>" data-nom-fichier="<?=$fichierCSV?>"><?= $fichierCSV ?></a></li>
+                                <?php endforeach; ?>
+                                </ul>
             <?php
-            endforeach;
-            ?></ul><?php
             closedir($sousDossier);
         endif;
-    endforeach;?>
-    </li><?php
-endif;?>
-</ul>
-            </div>
-            <div class="affichageInde"><p id="titreAffichage"></p></div>
-            <script src="./index.js"></script>
-            <script type="text/javascript" src="../papaparse.min.js"></script>
+    endforeach;
+    ?>
+
+                    </li>
+<?php endif; ?>
+
+            </ul>
+        </div>
+        <div class="affichageInde"><p id="titreAffichage"></p></div>
+        <script src="./index.js"></script>
+        <script type="text/javascript" src="../papaparse.min.js"></script>
     </body>
 </html>
